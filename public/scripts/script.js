5039a0e0675a0d79ab263b07e170d2fe772f3875
cryswerton <cryswertons@gmail.com>
2021-06-05 22:41:26 -0300

    added functions to get one or all the books to serve them as json

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,34 +6,6 @@ function print(something){
     console.log(something)
 }
 
-function Book(name = 'none', author = 'none', personalScore = 'none', finishReadingDate = 'none'){   
-    let obj = {
-        name: name,
-        author: author,
-        personalScore: personalScore,
-        finishReadingDate: finishReadingDate
-    }
-    return obj
-}
-
-books = [
-    // Name, Author, Personal Score Point and Finish Reading Date.
-    new Book('Eu, Robô', 'Isaac Asimov', 9.5, '04/2021'),
-    new Book('Hércules', 'Menelaos Stephanides', 8.5, '04/2021'),
-    new Book('Frankenstein', 'Mary Shelley', 8.0, '04/2021'),
-    new Book('As Cavernas de Aço', 'Isaac Asimov', 9.0, '05/2021' ),
-    new Book('Linux Essencial', 'Fábio J L Ferreira', 8.0, '05/2021'),
-    new Book('Coração de Aço', 'Brandon Sanderson', 10.0, '06/2021')
-]
-
-let html = ''
-
-for(let i = 0; i < books.length; i++){
-    html += books[i].name + '<br>'
-}
-
-print(html)
-
-content.innerHTML = html
+content.innerHTML = '<h1>Hello World</h1>'
 
 
--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const books = require('./books.js')
 const print = require('./print.js')
+
+// Gets all books
+app.get('/api/books', (req, res) => {
+    res.json(books)
+})
+
+// Gets a Single Book
+app.get('/api/books/:id', (req, res) => {
+    const found = books.some(book => book.id === parseInt(req.params.id))
+
+    if(found){
+        res.json(books.filter(book => book.id === parseInt(req.params.id)))
+    }else{
+        res.status(400).json({msg: `No book with the id of ${req.params.id}`})
+    }
+
+})
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => print(`Server started on port ${PORT}`))
 
